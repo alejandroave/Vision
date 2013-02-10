b4af911b3ef6b6a28872686469238194e0df29cc
alejandroave <alejandro.ave@gmail.com>
2013-02-09 22:51:51 -0600

    listo para martes

--- imagen.py
+++ imagen.py
@@ -6,52 +6,39 @@ from pygame.locals import *  ##Para los botones y presion del raton
 import Image                 ##para cargar imagenes, matriz de los pixeles
 from math import *           ##operaciones
 import sys                   ##cosas de sistema como para terminar el programa
-#from pygame import *
 import numpy                 ##arreglos
-#from numpy  import *
+import time
+import math
 
 Inombre = raw_input("Dame el nombre de la imagen con extencio: ")
-nimagen = 'nimagen.jpg'
-nfiltro = 'filtro.jpg'
-difusion = 'difusion.jpg'
-convolun = 'convolu.jpg'
-
-
+nnormal = 'nimagen.png'
+ngrises = 'filtro.png'
+nfiltro = 'difusion.png'
+nconvolucion = 'convolu.png'
+numbra = 'umbral.png'
+nnorma = 'normalizacion.jpg'
 min = 110.0 ##parametro definido
 max = 190.0 ##parametro definido
-im = Image.open(Inombre)   ##  cargar la imagen original
-ancho, altura = im.size    ##  Sacar ancho y altura de la imagenpara ajustar la ventana
 boton = pygame.Surface((100,25)) ##para crear el rectangulo del boton
 boton.fill((100,100,255))        ##para el color del boton
-#matriz = array([1,2,1],[1,5,1],[1,2,1])
-pixels = im.load()
-#matrix = ([-1,0,1],[-2,0,2],[-1,0,1])
-#matriy = ([1,2,1],[0,0,0],[-1,-2,-1])
 matrii = ([0,0,0],[-1,0,0],[0,0,0])
-print altura
 
-x = 2
-y = 2
-
-i=1
-j=1
-
-
+def cargar(imagen):
+	im = Image.open(imagen)
+	ancho, altura = im.size
+	pixels = im.load()
+	return ancho,altura,pixels,im
 
 
 
 ###funcion de filtro
-def filtro(nfiltro):
-	im = Image.open(Inombre) ##igual cargamos imagenes
-        ancho, altura = im.size  ##altura y ancho para los fors
-        pixels = im.load()       ##cargamos matriz de pixeles
-	prom = 0                 ##declaraicon de variable para promedio
-	altura = altura - 1      ##esto es para los for ya que inicia en cero
-	ancho = ancho - 1        ##esto es para los for ya que inicia en cero
+def filtro(imagen):
+	ancho,altura,pixels,im = cargar(imagen)
+	ancho = ancho -1
+	altura = altura -1
 	for j in range(altura):
 		for i in range(ancho):
 			if i == 0 and j == 0:
-				
 				prom = (sum(pixels[i + 1,j])/3 + sum(pixels[i,j + 1])/3 + sum(pixels[i,j])/3)/3
 				
 			#esquina superior derecha#
@@ -92,55 +79,58 @@ def filtro(nfiltro):
 			##guardamos para la nueva imagen
 			pixels[i,j] = (a,b,c)
 	##guardamos la nueva imagen
-	im.save(difusion)
+	im.save(nfiltro)
 	##enviamos la imagen ya cargada
-	return pygame.image.load(difusion)
+	return pygame.image.load(nfiltro)
 
 
-def convolucion(convolun):
-        im = Image.open(convolun) ##igual cargamos imagenes
-        ancho, altura = im.size  ##altura y ancho para los fors
-        pixels = list(im.getdata())       ##cargamos matriz de pixeles
-	pixels = im.load()
+def convolucion(imagen):
+	tiempoi = time.time()
+	print "Iniciando convolucion: "
+	ancho,altura,pixels,im = cargar(imagen)
         prom = 0                 ##declaraicon de variable para promedio
 	suma = 0
 	matrix = ([-1,0,1],[-2,0,2],[-1,0,1])
 	matriy = ([1,2,1],[0,0,0],[-1,-2,-1])
 	sumax = 0
 	sumay = 0
-	#altura = altura - 1
-	#ancho = ancho - 1
         for j in range(altura):
                 for i in range(ancho):
 			sumax = 0
 			sumay = 0
-			for y in range(3):
-				for x in range(3):
+			for x in range(len(matrix[0])):
+				for y in range(len(matrix[0])):
 					try:
-						multix = matrix[y][x]*pixels[i+y -1,j+x -1][1]
-						multiy = matriy[y][x]*pixels[i+y -1,j+x -1][1]
+						#multix = matrix[x][y]*pixels[i+y -1,j+x -1][1]
+						#multiy = matriy[x][y]*pixels[i+y -1,j+x -1][1]
+						multix = matrix[x][y]*pixels[i+y,j+x][1]
+                                                multiy = matriy[x][y]*pixels[i+y,j+x][1]
+
 					except:
 						multix = 0
 						multiy = 0
-				sumax = multix + sumax
-				sumay = multiy + sumay
-			suma = sumax + sumay
-			print suma
+					sumax = multix + sumax
+					sumay = multiy + sumay
+			xm = pow(sumax,2)
+                        ym = pow(sumay,2)
+                        g = int(math.sqrt(xm+ym))
+			suma = g
+			#suma = sumax + sumay
+			#print suma
 			if suma > 255:
 				suma = 255
 			if suma < 0:
 				suma = 0
 			pixels[i,j] = (suma,suma,suma)
-	im.save(convolun)
-	return pygame.image.load(convolun)
-
+	im.save(nconvolucion)
+	tiempof = time.time()
+        print "Se tardo: ",tiempof-tiempoi,"segundos"
+	return pygame.image.load(nconvolucion)
 
 
 ##funcion para la escala de grises
-def escala(Inombre):
-	im = Image.open(Inombre) ##igual que el de arriba cargamos la imagen
-        ancho, altura = im.size  ##anco y altura
-        pixels = im.load()       ##cargamos matriz de pixeles
+def escala(imagen):
+	ancho,altura,pixels,im = cargar(imagen)
 	for i in range(ancho):
         	for j in range(altura):
 			##sacamos promedio de (r,g,b) de los pixeles
@@ -151,14 +141,12 @@ def escala(Inombre):
 			b = prom ##igualamos
 			c = prom ##igualamos 	 		
 			pixels[i,j] = (a,b,c) ##igualamos
-        im.save(nfiltro) ##guardamos la imagen nueva
-	return pygame.image.load(nfiltro) ##enviamos la imagen cargada
+        im.save(ngrises) ##guardamos la imagen nueva
+	return pygame.image.load(ngrises) ##enviamos la imagen cargada
 
 ##funcion para el umbral
-def mumbra(Inombre):
-	im = Image.open(Inombre)  ##Aqui lo mismo que las dos funciones anteriores
-        ancho, altura = im.size
-        pixels = im.load()
+def mumbra(imagen):
+	ancho,altura,pixels,im = cargar(imagen)
 	for i in range(ancho):
 		for j in range(altura):
 			(a,b,c) = pixels[i,j] ##obtenemos valore
@@ -171,24 +159,52 @@ def mumbra(Inombre):
 			b = prom
 			c = prom
 			pixels[i,j]=(a,b,c)  ##asiganmos
-	im.save(nimagen)                     ##guardamos 
-	return pygame.image.load(nimagen)    ##regresemos la imagen cargada
+	im.save(numbra)                     ##guardamos 
+	return pygame.image.load(numbra)    ##regresemos la imagen cargada
 
 ##funcio para la diferenciacion entre la original y la imagen filtrada
-def diferencia(imagen1,imagen2):
-	im = Image.open(imagen1) ##cargamos todo el rollo para las dos imagenes
-	im1 = Image.open(imagen2) ##la original y la imagen filtrada
-	ancho,altura = im.size
-	pixels = im.load()
-	pixels1 = im1.load()
-	for i in range (ancho):
-		for j in range (altura):
+
+def normalisacion(imagen):
+	tiempoi = time.time()
+	a = 0
+	b = 255
+	c = 5
+	d = 95
+	res = 0
+	ancho,altura,pixels,im = cargar(imagen)
+	for j in range(altura):
+		for i in range(ancho):
+			res = (pixels[i,j][1] - c)*((b-a)/(c - d)) + 255
+			
+			if res > 100:
+				res = 255
+			else:
+				res = 0
+			#if res > 255:
+			#	res = 255
+			#if res < 0:
+			#	res = 0
+			#pixels[i,j]=(res,res,res)	
+	im.save(nnorma) ##guardamos imagenes
+        tiempof = time.time()
+	print "tiempo de normalizacion: ",tiempof - tiempoi
+        return pygame.image.load(nnorma)		
+
+
+
+def diferencia(imagen1,imagen2,min,max):
+	ancho,altura,pixels,im = cargar(imagen1)
+        ancho,altura,pixels,im = cargar(imagen2)
+	for i in range (ancho -2):
+		for j in range (altura -2):
 			(a,b,c) = pixels[i,j] ##guardamos pixeles de ambas 
 			(a1,b1,c1) = pixels1[i,j] ##imagenes
 			prom = a+b+c/3            ##sacamos propedio de ambas
 			prom1 = a1+b1+c1/3
 			prom2 = prom - prom1      ##restamos
-			if prom2 > 200:            ##aqui asemos binarisasion
+			v = prom2*255/500
+			print v
+			if v > 25:            ##aqui asemos binarisasion
 				prom2 = 255
 			else:
 				prom2 = 0
@@ -205,6 +221,7 @@ def diferencia(imagen1,imagen2):
 ##funcion principla del programa
 def main(nombreI):
 	pygame.init() ##inicialisamos python
+	ancho,altura,pixels,im = cargar(nombreI)
 	pantalla = pygame.display.set_mode((ancho + 150,altura)) ##inicialisamos pantalla
 	pygame.display.set_caption("Rutinas para imagenes") ##ponemos nombre
 	imagen = pygame.image.load(nombreI) ##cargamos imagen principal a mostrar
@@ -229,14 +246,19 @@ def main(nombreI):
 						imagen = escala(nombreI) ##hace llamar a la funcion de escala y garda el resultado
 						grisle = 'Filtro'
 					if cont == 1:
-						imagen = filtro(nfiltro) ##lo mismo que arriba pero para filtro
+						imagen = filtro(ngrises) ##lo mismo que arriba pero para filtro
 						grisle = 'convolucion'
 					if cont == 2:
-						imagen = convolucion(difusion) ##lo mismo de arriba para difucion
-						grisle = 'normal'
-					cont = cont + 1	
+						imagen = convolucion(nfiltro) ##lo mismo de arriba para difucion
+						grisle = 'normalizacion'
 					if cont == 3:
+                                                imagen = normalisacion(nconvolucion) ##lo mismo de arriba para$
+                                                grisle = 'normal'
+                                       # cont = cont + 1	
+					if cont == 4:
+						imagen = pygame.image.load(nombreI)
 						cont = 0
+					cont = cont + 1
 				##accion para el boton de umbral
 
 				if 0 < cordx < 100 and 30 < cordy < 60:
